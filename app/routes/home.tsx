54e61d52b3034dfa8d4ba3531d1d2b9af52f62d6
Huzi7869 <168625375+Huzi7869@users.noreply.github.com>
2025-08-12 10:34:01 +0200

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import NavBar from "~/Components/NavBar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "ResuMate" },
+        { name: "description", content: "Smart Feedback For Your Dream Job!" },
+    ];
 }
-
 export default function Home() {
-  return <Welcome />;
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover items-center justify-center">
+            <NavBar />
+
+            <section className="main-section">
+                <div className="page-heading py-16">
+                    <h1>Track Your Applications & Resume Ratings</h1>
+                    <h2>Review your submissions and check AI-Powered Feedback</h2>
+                </div>
+
+                {resumes.length > 0 && (
+                    <div>
+                        {resumes.map((resume) => (
+                            <ResumeCard key={resume.id} resume={resume}/>
+                        ))}
+                    </div>
+                )}
+            </section>
+
+        </main>
+    )
 }
